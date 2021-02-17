6f75c636156c5a0a38359773c070ac489a064783
gujie01@cn.panasonic.com <gujie01@cn.panasonic.com>
2021-02-17 14:33:01 +0800

    这是第一个版本

---
+++ src/main/java/com/atguigu/boot/MainApplication.java
@@ -0,0 +1,20 @@
+package com.atguigu.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        //1.返回IOC容器
+        ConfigurableApplicationContext run = SpringApplication.run(MainApplication.class, args);
+
+        //2.查看容器里面的组件
+        String[] names = run.getBeanDefinitionNames();
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
---
+++ src/main/java/com/atguigu/boot/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.atguigu.boot.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@ResponseBody
+//@Controller
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String handle01() {
+        return "Hello, Spring Boot 2!";
+    }
+}
